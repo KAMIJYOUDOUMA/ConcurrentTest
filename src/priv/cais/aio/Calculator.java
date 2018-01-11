880d5a059b3e8d71d38369c9870a42b6af3a9b15
KAMIJYOUDOUMA <1246846408@qq.com>
2018-01-11 18:21:26 +0800

    AIO

---
+++ src/priv/cais/aio/Calculator.java
@@ -0,0 +1,16 @@
+package priv.cais.aio;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * @auther CaiS
+ */
+public final class Calculator {
+    private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
+
+    public static Object cal(String expression) throws ScriptException {
+        return jse.eval(expression);
+    }
+}
